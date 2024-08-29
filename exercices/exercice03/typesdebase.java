f28eec8fde7b7686c9e06de8e1f7fdfc6a3c0377
nguyen-kenny-emf <kennyhoangkhoa.nguyen@studentfr.ch>
2024-08-29 11:40:42 +0200

    test

--- exercices/exercice03/typesdebase.java
+++ exercices/exercice03/typesdebase.java
@@ -2,7 +2,6 @@ public class typesdebase {
 
     public static void main(String[] args) {
     int age = 0;  
-    int nombre;   
     String nom = "kenny";
     
     System.out.println("je m'appelle " + nom + " et j'ai " + age + " ans.");
