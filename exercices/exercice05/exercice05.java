23fe0aa2d0df84a9e4edc3b67820c7bb53cab79e
nguyen-kenny-emf <kennyhoangkhoa.nguyen@studentfr.ch>
2024-09-12 10:51:18 +0200

    premiere partie exercice05

---
+++ exercices/exercice05/exercice05.java
@@ -0,0 +1,17 @@
+package exercice05 ;
+    public class exercice05 {
+    
+    
+    public static void main(String[] args) {
+        int valeurATester = -13;
+        boolean positif = (valeurATester >= 0);
+
+
+        if (positif) {
+            System.out.println("La valeur est positive"); 
+        } else { 
+            System.out.println("la valeur est negative");
+        }
+        
+    } 
+}
