ca46f9863de1c97fdaec84cb4d0ae2ed6f890352
nguyen-kenny-emf <kennyhoangkhoa.nguyen@studentfr.ch>
2024-09-19 09:43:43 +0200

    condition3 exercice05

---
+++ exercices/exercice05/condition3.java
@@ -0,0 +1,18 @@
+public class condition3 {
+    public static void main(String[] args) {
+        int valeur1 = -13;
+        int valeur2 = 3;
+        int multiplication = (valeur1 * valeur2);
+
+        boolean réponse;   
+        réponse = (multiplication < 0);
+    
+    if (réponse) {
+        System.out.println("la valeur est négative");
+    }else{
+        System.out.println("la valeur est positive");
+    }
+        } 
+        }
+        
+
