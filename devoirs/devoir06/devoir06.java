72def0598bfe50f7b1d5258df27a88ffb0d8e779
nguyen-kenny-emf <kennyhoangkhoa.nguyen@studentfr.ch>
2024-11-07 11:46:10 +0100

    devoir06 pas fini

---
+++ devoirs/devoir06/devoir06.java
@@ -0,0 +1,18 @@
+package devoirs.devoir06;
+
+public class devoir06 {
+    public static final int TAILLE_TABLEAU = 20;
+    public static final int VALEUR_MIN = 0;
+    public static final int VALEUR_MAX = 50; 
+    public static final int VALEUR_RECHERCHEE = 7;
+    public static void main(String[] args) {
+        
+    }
+    public static int[] genereTableau(int min, int max, int taille) {
+        int [] tableau = new int[taille];
+
+        return tableau;
+
+    }
+}
+
