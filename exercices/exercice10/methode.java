01c02b2c7cfe93f182e7baee79252730c1821425
nguyen-kenny-emf <kennyhoangkhoa.nguyen@studentfr.ch>
2024-11-07 08:29:21 +0100

    exercice 31 octobre

---
+++ exercices/exercice10/methode.java
@@ -0,0 +1,10 @@
+public class methode {
+    public static void main(String[] args) {
+    Halloween();
+    }
+    public static void Halloween(){
+    System.out.println("BOUH!");
+    }
+    
+
+}
---
+++ exercices/exercice10/methodes.java
@@ -0,0 +1,10 @@
+public class methodes {
+    public static void main(String[] args) {
+    Halloween();
+    }
+    public static void Halloween(){
+    System.out.println("BOUH!");
+    }
+    
+
+}
