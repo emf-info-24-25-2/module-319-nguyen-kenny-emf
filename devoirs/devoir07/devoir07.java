bf2c58faae1b4c22795a48c91fb47f29526160d7
nguyen-kenny-emf <kennyhoangkhoa.nguyen@studentfr.ch>
2024-11-14 11:44:27 +0100

    devoir07

---
+++ devoirs/devoir07/devoir07.java
@@ -0,0 +1,13 @@
+public class devoir07 {
+
+    public static void main(String[] args) {
+        final int TAILLE_TABLEAU = 10;
+        final int MIN_NOMBRE = -8;
+        final int MAX_NOMBRE = 8;
+        final int VALEUR_RECHERCHEE = 7;
+    }
+    public static void genererValeursAleatoires() {
+        m
+        
+    }
+}
