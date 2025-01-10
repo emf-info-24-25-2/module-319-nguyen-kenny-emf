04449d32d5a8ad12a9d396c6b695686ab97817e3
nguyen-kenny-emf <kenny.nguyen@studentfr.ch>
2025-01-10 12:52:07 +0100

    devoir05

--- devoirs/devoir05/devoir05.java
+++ devoirs/devoir05/devoir05.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class devoir05 {
 
     public static void main(String[] args) {
+        int nombreMystere = (int) (Math.random() * (100 - 1 + 1)) + 1;
         
+        Scanner scanner = new Scanner( System.in );
+        int valeur ;
+        do{
+        System.out.println("Veuillez entrer un nombre :");
+        valeur = scanner.nextInt();
+            if (valeur == nombreMystere) {
+                System.out.println("Bravo, trouvé!");
+            } else if (valeur < nombreMystere){
+                System.out.println("trop petit!");
+            } else if (valeur > nombreMystere){
+                System.out.println("trop grand !");
+            }
+        } while (valeur != nombreMystere);
+        scanner.close();
     }
 }
